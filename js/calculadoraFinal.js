6d777d1bfd3325443b0f270e46dbfb8f6062c509
JoffreDavid <jdariasb@uce.edu.ec>
2025-11-12 14:17:28 -0500

    Calculadora Funcional

--- js/calculadoraFinal.js
+++ js/calculadoraFinal.js
@@ -1,7 +1,64 @@
-let num1 = 0;
-let num2 = 0;
+let display = document.getElementById('display');
+let num1 = null, operador = null;
 
 function mostrarDisplay(valor) {
-    let elemento = document.getElementById('display');
-    elemento.innerText = elemento.innerText + valor;
+    if (!isNaN(valor) || valor === '.') {
+        display.innerText += valor;
+    }
+
+    else if (['+', '-', '*', '/'].includes(valor)) {
+        if (display.innerText === '' && valor === '-') {
+            display.innerText = '-';
+            return;
+        }
+
+        if (num1 === null) {
+            num1 = parseFloat(display.innerText);
+        } else if (operador) {
+            let num2 = parseFloat(display.innerText);
+            num1 = calcular(num1, num2, operador);
+            display.innerText = num1;
+        }
+
+        operador = valor;
+        display.innerText += valor; 
+    }
+
+    else if (valor === '%') {
+        if (display.innerText) {
+            let actual = parseFloat(display.innerText);
+            display.innerText = operador && num1 ? (num1 * actual) / 100 : actual / 100;
+        }
+    }
+
+    else if (valor === '=') {
+        if (operador !== null) {
+            let partes = display.innerText.split(/[\+\-\*\/]/);
+            let num2 = parseFloat(partes.pop());
+            let resultado = calcular(num1, num2, operador);
+            display.innerText = resultado;
+            num1 = resultado;
+            operador = null;
+        }
+    }
+
+    else if (valor === 'C') {
+        display.innerText = '';
+        num1 = null;
+        operador = null;
+    }
+
+    else if (valor === '←') {
+        display.innerText = display.innerText.slice(0, -1);
+    }
+}
+
+function calcular(a, b, op) {
+    switch (op) {
+        case '+': return a + b;
+        case '-': return a - b;
+        case '*': return a * b;
+        case '/': return b !== 0 ? a / b : "Error";
+        default: return b;
+    }
 }
